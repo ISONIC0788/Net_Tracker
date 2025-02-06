1111c82e78173a789f73f10b2e68bf4929031e0a
EbediMeleck <ituzeebedi12@gmail.com>
2025-02-06 06:37:51 -0800

    init message;

---
+++ main.c
@@ -0,0 +1,9 @@
+ #include<stdio.h>
+int main(){
+	char name[20] ;
+	printf("Hellow User !!!! \n");
+	printf("please Enter your name Here\n");
+	scanf("%s",&name);
+	printf("I'm Exctiry To Meet with you::\'%s\'",name);
+	return 0 ;
+}
